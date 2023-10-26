cdec54d8b782eec80a1771494349ba671b58381d
khoahuynhdev <khoa0319@gmail.com>
2023-10-26 21:08:48 +0700

    feat: test on oct 26 2023

---
+++ 705__design_hashset.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MyHashSet struct {
+	bucketArray []bool
+}
+
+func Constructor() MyHashSet {
+	// constraint 0 <= key <= 106
+	// At most 104 calls will be made to add, remove, and contains.
+	return MyHashSet{make([]bool, 1000001)}
+}
+
+func (hs *MyHashSet) Add(key int) {
+	hs.bucketArray[key] = true
+}
+
+func (hs *MyHashSet) Remove(key int) {
+	hs.bucketArray[key] = false
+}
+
+func (hs *MyHashSet) Contains(key int) bool {
+	return hs.bucketArray[key]
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
+func main() {
+	mySet := Constructor()
+
+	mySet.Add(1)
+	fmt.Println(mySet.Contains(1))
+}
