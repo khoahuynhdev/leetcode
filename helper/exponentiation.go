7c66b1932a6afe277335c5204a46304e7b3024dc
khoahuynhdev <khoa0319@gmail.com>
2023-12-03 14:02:37 +0700

    feat: improve fast exp, lc-50

--- helper/exponentiation.go
+++ helper/exponentiation.go
@@ -7,8 +7,12 @@ func FastExp(a, n int) uint64 {
 	if n == 1 {
 		return uint64(a)
 	}
+	if n < 0 {
+		return 1 / FastExp(a, -n)
+	}
 	if n%2 == 0 {
-		return FastExp(a, n/2) * FastExp(a, n/2)
+		res := FastExp(a, n/2)
+		return res * res
 	}
 	return FastExp(a, n-1) * uint64(a)
 }
