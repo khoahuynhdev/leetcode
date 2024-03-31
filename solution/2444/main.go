1ec74c1574685b662e24f40416b9c5eea1a3c6b0
Khoa Huỳnh (Daniel) <khoa0319@gmail.com>
2024-03-31 23:44:06 +0700

    Create main.go

---
+++ solution/2444/main.go
@@ -0,0 +1,26 @@
+package main
+
+func countSubarrays(nums []int, minK int, maxK int) int64 {
+    var res int64
+	left := 0
+	pmin := -1
+	pmax := -1
+
+	for right, num := range nums {
+		if num < minK || num > maxK {
+			left = right + 1
+			pmin = -1
+			pmax = -1
+		} else {
+			if num == minK {
+				pmin = right
+			}
+			if num == maxK {
+				pmax = right
+			}
+			res += int64(max(0, min(pmin, pmax) - left + 1))
+		}
+	}
+
+	return res
+}
